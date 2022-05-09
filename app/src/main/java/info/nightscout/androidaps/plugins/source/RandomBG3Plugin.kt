5c492fb46f952954cfaeaf8b9950b7376d98ac1b
ZzPingoleon <93032866+ZzPingoleon@users.noreply.github.com>
2022-05-09 15:46:47 +0200

    Update RandomBG3Plugin.kt

--- app/src/main/java/info/nightscout/androidaps/plugins/source/RandomBG3Plugin.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/source/RandomBG3Plugin.kt
@@ -62,9 +62,9 @@ object RandomBG3Plugin : PluginBase(PluginDescription()
         if (!isEnabled(PluginType.BGSOURCE)) return
         val cal = GregorianCalendar()
         val currentMinute = cal.get(Calendar.MINUTE) + (cal.get(Calendar.HOUR_OF_DAY) % 2) * 60
-        var bgMgdl = 100+currentMinute*0.2
+        var bgMgdl = 100+cal.get(Calendar.MINUTE)*0.2
         if (currentMinute%20<10){
-          bgMgdl = 100-currentMinute*0.2
+          bgMgdl = 100-cal.get(Calendar.MINUTE)*0.2
         }
         
         val bgReading = BgReading()
