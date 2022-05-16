75ec109eb02744380c2b6ca9fc396dec3eb4a363
ZzPingoleon <93032866+ZzPingoleon@users.noreply.github.com>
2022-05-16 08:45:05 +0200

    Update and rename ManualPlugin.java to ManualPlugin.kt

---
+++ app/src/main/java/info/nightscout/androidaps/plugins/source/ManualPlugin.kt
@@ -0,0 +1,75 @@
+package info.nightscout.androidaps.plugins.source
+
+import android.content.Intent
+import android.os.Handler
+import info.nightscout.androidaps.MainApp
+import info.nightscout.androidaps.R
+import info.nightscout.androidaps.db.BgReading
+import info.nightscout.androidaps.interfaces.BgSourceInterface
+import info.nightscout.androidaps.interfaces.PluginBase
+import info.nightscout.androidaps.interfaces.PluginDescription
+import info.nightscout.androidaps.interfaces.PluginType
+import info.nightscout.androidaps.logging.L
+import info.nightscout.androidaps.plugins.pump.virtual.VirtualPumpPlugin
+import info.nightscout.androidaps.utils.DateUtil
+import info.nightscout.androidaps.utils.T
+import info.nightscout.androidaps.utils.isRunningTest
+import info.nightscout.androidaps.utils.SP
+import org.slf4j.LoggerFactory
+import java.util.*
+import kotlin.math.PI
+import kotlin.math.sin
+
+object ManualPlugin : PluginBase(PluginDescription()
+        .mainType(PluginType.BGSOURCE)
+        .fragmentClass(BGSourceFragment::class.java.name)
+        .pluginName(R.string.source_manual)
+        .preferencesId(R.xml.pref_manual)
+        .description(R.string.description_source_manual)), BgSourceInterface {
+
+    private val log = LoggerFactory.getLogger(L.BGSOURCE)
+
+    private val loopHandler = Handler()
+    private lateinit var refreshLoop: Runnable
+
+    const val interval = 1L // minutes
+
+    init {
+        refreshLoop = Runnable {
+            handleNewData(Intent())
+            loopHandler.postDelayed(refreshLoop, T.mins(interval).msecs())
+        }
+    }
+
+    override fun advancedFilteringSupported(): Boolean {
+        return true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loopHandler.postDelayed(refreshLoop, T.mins(interval).msecs())
+    }
+
+    override fun onStop() {
+        super.onStop()
+        loopHandler.removeCallbacks(refreshLoop)
+    }
+
+    override fun specialEnableCondition(): Boolean {
+        return isRunningTest()
+    }
+
+    override fun handleNewData(intent: Intent) {
+        if (!isEnabled(PluginType.BGSOURCE)) return
+           
+        Double bg_manual_default=100.0
+        Double bg_manual = SP.getDouble(R.string.key_manual_constant, bg_manual_default)
+
+        val bgReading = BgReading()
+        bgReading.value = bg_manual
+        bgReading.date = DateUtil.now()
+        bgReading.raw = bg_manual
+        MainApp.getDbHelper().createIfNotExists(bgReading, "Manual")
+        log.debug("Generated BG: $bgReading")
+    }
+}
