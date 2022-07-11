f1297f076ae797e53937f32218c19045636e589c
ZzPingoleon <93032866+ZzPingoleon@users.noreply.github.com>
2022-07-11 09:03:01 +0200

    Update DetermineBasalResultAMA.java

--- app/src/main/java/info/nightscout/androidaps/plugins/aps/Fit4FIT/DetermineBasalResultAMA.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/aps/Fit4FIT/DetermineBasalResultAMA.java
@@ -1,4 +1,4 @@
-package info.nightscout.androidaps.plugins.aps.DynamicBolusCalculator;
+package info.nightscout.androidaps.plugins.aps.Fit4FIT;
 
 import org.json.JSONException;
 import org.json.JSONObject;
